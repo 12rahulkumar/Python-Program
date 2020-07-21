75573b1e489728501b97c12315778748b3bc00c6
12rahulkumar <rahulasher0@gmail.com>
2020-07-21 20:24:21 +0530

    learn python by writing code

---
+++ 66th_Python_Code.py
@@ -0,0 +1,22 @@
+a = 20
+b = 3
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+
+s1 = "rahul"
+s2 = "rahul"
+
+print(s1>s2)
+print(s1<s2)
+print(s1!=s2)
+print(s1==s2)
+print(s1<=s2)
+print(s1>=s2)
+
+print(True<True)
+print(True<False)
+print(True<=False)
+print(False>=True)
+print(True>=True)
